a8d7e0b960ceb99345997d252b054989df5b670d
Tom George <grigorytomgeorge@gmail.com>
2020-09-05 18:33:06 +0530

    More Cool Python Automation

---
+++ More Python Automations/Computer Health Tester/healthchecker.py
@@ -0,0 +1,17 @@
+import shutil
+import psutil
+
+
+def check_disk_usage(disk):
+    du = shutil.disk_usage(disk)
+    free = du.free/du.total * 100
+    return free > 20
+
+def check_cpu_usage():
+    usage = psutil.cpu_percent(0.1)
+    return usage > 75
+
+if not check_disk_usage("/") or check_cpu_usage():
+    print("Youre Computer is in Danger.")
+else:
+    print("Everything is Awesome!!!!!")    
